36ddd6a93b82a7e7cc39f8df6bafd27a5cf7037b
y-matsuno-u <chyu24003@g.nihon-u.ac.jp>
2025-11-18 17:26:26 +0900

    標準偏差のコード追加

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ import scipy.linalg #時間発展演算子の作成で利用
 import matplotlib.pyplot as plt
 import random
 #パラメータ
-L = 10
+L = 100
 l = 2*np.pi
 epsilon = l / L
 p = 1
@@ -33,22 +33,6 @@ def is_hermitian(matrix):
 
 def beta(j,L,pos,epsilon):
     return 0
-    width = 1
-    A = 0.6
-    jh = int(L/3)
-    if pos == "lr":
-        # β = -1 を j = 71 で踏むように調整
-        return -A*np.tanh(3/width*(j - 2*jh - 0.730833344)*epsilon) - 0.6
-    elif pos == "ur":
-        # β = +1 を j = 70 で踏むように調整
-        return  A*np.tanh(3/width*(j - 2*jh + 0.269166656056467)*epsilon) + 0.6
-        # （注）式は (j - center - c) なので c = -0.269... は “+0.269...” と等価
-    elif pos == "ll":
-        # β = -1 を j = 29 で踏むように調整
-        return  A*np.tanh(3/width*(j - jh - 0.269166656056467)*epsilon) - 0.6
-    elif pos == "ul":
-        # β = +1 を j = 29 で踏むように調整
-        return -A*np.tanh(3/width*(j - jh - 0.269166656056467)*epsilon) + 0.6
 
 for i in range(2*L):
     for j in range(2*L):
@@ -103,58 +87,7 @@ eigenvalues, eigenvectors = LA.eigh(H_BdG)
 #固有値、固有ベクトルのソート(確認済み)
 eigenvalues = np.concatenate((eigenvalues[L:], eigenvalues[:L][::-1]), 0)
 eigenvectors = np.concatenate((eigenvectors[:,L:], eigenvectors[:,:L][:,::-1]), 1)
-# print("固有ベクトル（旧）")
-# for i in range(L):
-#     print(eigenvalues[i])
-#     print(eigenvectors[:,i])
-#     print(eigenvalues[L+i])
-#     print(eigenvectors[:,L+i])
-# #ゼロモード(0とL)入れ替え
-# tmp = eigenvectors[:,0].copy()
-# eigenvectors[:,0] = eigenvectors[:,L].copy()
-# eigenvectors[:,L] = tmp
 
-#粒子-反粒子対称性を満たすように固有ベクトルを調整(0~L-1成分でL~2L-1成分を作る)
-# cnt = []
-# for i in range(L):
-#     threshold = 1e-10
-#     ans = eigenvectors[:,i+L] + np.concatenate((eigenvectors[L:,i].conj(), eigenvectors[:L,i].conj()), 0)
-#     if np.all(np.abs(ans) < threshold):
-#         #continue
-#         print(str(i))
-#         cnt.append(1)
-#         #print(np.where(np.abs(ans) < threshold, 0.0, ans))
-#     else:
-#         print(str(i)+"'")
-#         ans = eigenvectors[:,i+L] - np.concatenate((eigenvectors[L:,i].conj(), eigenvectors[:L,i].conj()), 0)
-#         if np.all(np.abs(ans) < threshold):
-#             cnt.append(-1)
-#             continue
-#             print(np.where(np.abs(ans) < threshold, 0.0, ans))
-#         else:
-#             cnt.append(0)
-#             print(str(i)+"''")
-#             ans = eigenvectors[:,i+L].T.conj() @ np.concatenate((eigenvectors[L:,i].conj(), eigenvectors[:L,i].conj()), 0)
-#             print(np.where(np.abs(ans) < threshold, 0.0, ans))
-# plt.plot(cnt)
-# plt.show()
-# bad_idx = []
-# for i in range(L-1):
-#     # i と i+1 が 1,-1 または -1,1 のペアならOK
-#     if cnt[i] in (1, -1) and cnt[i+1] in (1, -1) and cnt[i+1] == -cnt[i]:
-#         continue
-#     # それ以外は「交互じゃない」とみなして記録
-#     bad_idx.append(i)
-
-# print("1,-1 が交互になっていない位置 i (ペア: i, i+1):", bad_idx)
-# d_cnt = []
-# for i in range(L-1):
-#     if abs(cnt[i+1] - cnt[i]) -2 < 1e-5:
-#         d_cnt.append(1)
-#     else:
-#         d_cnt.append(0)
-# plt.plot(d_cnt)
-# plt.show()
 V = np.zeros((2*L, 2*L), dtype=complex)
 for i in range(L):
     V[:,i] = eigenvectors[:,i]
@@ -164,24 +97,6 @@ eigenvectors = V
 for i in range(2*L):
     print(eigenvalues[i])
     print(eigenvectors[:,i])
-# print()
-# print("固有ベクトル（新）")
-# for i in range(L):
-#     print(eigenvalues[i])
-#     print(eigenvectors[:,i])
-#     print(eigenvalues[L+i])
-#     print(eigenvectors[:,L+i])
-# import sys
-# sys.exit()
-# for i in range(L):
-#     print("固有値")
-#     print(eigenvalues[i])
-#     print("固有ベクトル")
-#     print(eigenvectors[:,i])
-#     print("固有値")
-#     print(eigenvalues[i+L])
-#     print("固有ベクトル")
-#     print(eigenvectors[:,i+L])
 
 #cj_dag_cj
 cj_dag_cj_list = []
@@ -219,26 +134,20 @@ weights[~mask] = 0
 
 weights /= np.linalg.norm(weights)
 
-
-# plt.plot(np.r_[weights, weights[0]])
-# plt.title("weights")
-# plt.grid()
-# plt.show()
-
 for j in range(L):
     for n in range(L):
         #cを置く場合
-        psi[n, 0] += weights[j] * (eigenvectors[j,n].conj())
+        #psi[n, 0] += weights[j] * (eigenvectors[j,n].conj())
         #+
-        # if pos == "ur" or pos == "lr":
-        #     psi[n, 0] += weights[j] * (
-        #     1/np.sqrt(2) * (np.exp(1j*np.pi/4) * eigenvectors[j,n+L] + np.exp(-1j*np.pi/4) * eigenvectors[j,n].conj())
-        # )
-        # #-
-        # else:
-        #     psi[n, 0] += weights[j] * (
-        #     1/np.sqrt(2) * (np.exp(-1j*np.pi/4) * eigenvectors[j,n+L] + np.exp(1j*np.pi/4) * eigenvectors[j,n].conj())
-        # )
+        if pos == "ur" or pos == "lr":
+            psi[n, 0] += weights[j] * (
+            1/np.sqrt(2) * (np.exp(1j*np.pi/4) * eigenvectors[j,n+L] + np.exp(-1j*np.pi/4) * eigenvectors[j,n].conj())
+        )
+        #-
+        else:
+            psi[n, 0] += weights[j] * (
+            1/np.sqrt(2) * (np.exp(-1j*np.pi/4) * eigenvectors[j,n+L] + np.exp(1j*np.pi/4) * eigenvectors[j,n].conj())
+        )
 #状態ベクトルの規格化
 psi /= np.linalg.norm(psi)
 
